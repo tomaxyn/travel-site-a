cefd6a59bebbfddba4a20166986d3763d5e627e3
Cesar <ackwebroad@gmail.com>
2016-11-12 20:50:04 -0600

    Completed CSS of the header

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp'),postex = require('gulp-postcss'), prefixer = require('autoprefixer')
 	varicss = require('postcss-simple-vars'), nestnow = require('postcss-nested'),
-	importx = require('postcss-import'), mezclas = require('postcss-mixins');
+	importx = require('postcss-import'), mezclas = require('postcss-mixins'),
+	colorcomb = require('postcss-hexrgba');
 	
 
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/style.css')
-	.pipe(postex([importx, mezclas, varicss, nestnow, prefixer]))
+	.pipe(postex([importx, mezclas, varicss, nestnow, colorcomb, prefixer]))
 	.on('error', function(errorInfo){
 		console.log(errorInfo.toString());
 		this.emit('end');
