fa64f9fe572a426c4e1ffd01ad30d18c259eb06a
Cesar <ackwebroad@gmail.com>
2016-11-12 18:44:25 -0600

    Completed automatic sprite generation

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,12 @@
 var gulpo = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
+var grename = require('gulp-rename');
+var deletion = require('del');
 
 var config = {
 	mode: {
 		css:{
+			sprite: 'sprite.svg',
 			render:{
 				css: {
 					template: './gulp/templates/sprite.css'
@@ -12,11 +15,32 @@ var config = {
 		}
 	}
 }
-gulpo.task('createSprite', function(){
+
+
+gulpo.task('beginClean', function(){
+	return deletion(['.app/temp/sprite', './app/assets/images/sprites']);
+});
+
+gulpo.task('createSprite', ['beginClean'], function(){
 	return gulpo.src('./app/assets/images/icons/**/*.svg')
 		.pipe(svgSprite(config))
 		.pipe(gulpo.dest('./app/temp/sprite/'));
-
 });
 
+gulpo.task('copySpriteGraphic', ['createSprite'], function() {
+	return gulpo.src('./app/temp/sprite/css/**/*.svg')
+			.pipe(gulpo.dest('./app/assets/images/sprites'));
+});
+
+gulpo.task('copySpriteCSS', ['createSprite'], function(){
+	return gulpo.src('./app/temp/sprite/css/*.css')
+		.pipe(grename('_sprites.css'))
+		.pipe(gulpo.dest('./app/assets/styles/modules'));
+});
+
+gulpo.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function(){
+	return deletion('./app/temp/sprite');
+});
+
+gulpo.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
 
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -6,7 +6,7 @@ gulp.task('watch', function(){
 	browserSync.init({
 		notify: false,
 		server: {
-			baseDir: './app'
+			baseDir: 'app'
 		}
 	});
 	watcha('./app/index.html', function(){		
