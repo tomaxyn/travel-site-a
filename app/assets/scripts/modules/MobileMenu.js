2705ff8e611a497efa017a933a25aac43ff27c76
Cesar <ackwebroad@gmail.com>
2016-11-13 21:07:06 -0600

    funcionalidad y estilos del menu mobile

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 
 class MobileMenu {
 	constructor(){
+		this.siteHeader = $('.site-header');
 		this.menuIcon = $('.site-header__menu-icon');
 		this.menuContent = $('.site-header__menu-content');
 		this.events();
@@ -14,7 +15,9 @@ class MobileMenu {
 
 	toggleTheMenu() {
 		
-		this.menuContent.toggleClass('site-header__menu-content--is-visible');		
+		this.menuContent.toggleClass('site-header__menu-content--is-visible');
+		this.siteHeader.toggleClass('site-header--is-expanded');
+		this.menuIcon.toggleClass('site-header__menu-icon--close-x');
 	}
 }
 
