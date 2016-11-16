b8ecae4511ea70ddc1f82b859d9aaa854263d316
Cesar <ackwebroad@gmail.com>
2016-11-16 16:05:03 -0600

    replay error - p2

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -11222,7 +11222,7 @@
 		_createClass(StickyHeader, [{
 			key: 'refreshWaypoints',
 			value: function refreshWaypoints() {
-				this.lazyImages.load(function () {
+				this.lazyImages.on("load", function () {
 					Waypoint.refreshAll();
 				});
 			}
